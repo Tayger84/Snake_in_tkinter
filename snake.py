de92665e0ba5efae97e3565a3d20b85371afc3e8
Tayger84 <143450458+Tayger84@users.noreply.github.com>
2025-02-05 06:23:54 +0100

    Canvas object + snake rectangle

--- snake.py
+++ snake.py
@@ -1,1 +1,12 @@
 from tkinter import *
+
+canvas = Canvas(width=400, height=400)
+canvas.pack()
+
+
+def snake():
+    canvas.create_rectangle(100, 100, 110, 110, fill="black")
+
+snake()
+
+mainloop()
