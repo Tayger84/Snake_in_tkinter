a13588bf9737446ff97186823abb6a63bb9f9eff
Tayger84 <143450458+Tayger84@users.noreply.github.com>
2025-02-05 21:28:04 +0100

    Canvas + main window definition and buttons and Score Label

--- snake.py
+++ snake.py
@@ -1,12 +1,31 @@
 from tkinter import *
+# main window
+window = Tk()
+window.configure(bg="#73f094")
+window.title("Snake Game")
+window.iconbitmap("snake.ico")
 
-canvas = Canvas(width=400, height=400)
-canvas.pack()
+# score label for actual state of score
+score = Label(window, text="Score: 0", font=("Helvetica", 32, "bold"), bg="#73f094")
+score.grid(row=0, column=1)
 
+# canvas for snake area
+canvas = Canvas(window, width=400, height=400, bg="white")
+canvas.grid(row=1, column=1)
 
+# Buttons control via mause
+w = Button(window, text=" W ", command="", width=4).grid(row=2, column=1)
+a = Button(window, text=" A ", command="", width=4).grid(row=3, column=0)
+s = Button(window, text=" S ", command="", width=4).grid(row=3, column=1)
+d = Button(window, text=" D ", command="", width=4).grid(row=3, column=2)
+
+# snake function
 def snake():
     canvas.create_rectangle(100, 100, 110, 110, fill="black")
 
+# snake initialization
 snake()
 
-mainloop()
+
+# main loop
+window.mainloop()
