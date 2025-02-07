edc8be24461eb912c3007e718925bad487a6c612
Tayger84 <143450458+Tayger84@users.noreply.github.com>
2025-02-07 17:02:28 +0100

    Finished design of the game aplication

--- snake.py
+++ snake.py
@@ -4,20 +4,26 @@ window = Tk()
 window.configure(bg="#73f094")
 window.title("Snake Game")
 window.iconbitmap("snake.ico")
+window.resizable(False, False)
 
 # score label for actual state of score
 score = Label(window, text="Score: 0", font=("Helvetica", 32, "bold"), bg="#73f094")
-score.grid(row=0, column=1)
+score.grid(row=0)
 
 # canvas for snake area
-canvas = Canvas(window, width=400, height=400, bg="white")
-canvas.grid(row=1, column=1)
+canvas = Canvas(window, width=400, height=400, bg="white", highlightthickness=3, 
+    highlightbackground='grey', relief="groove")
+canvas.grid(row=1, padx=10, pady=10)
+
+# Frame for Buttons
+buttons_group = Frame(window, bg="#73e090")
+buttons_group.grid(row=2, pady=10)
 
 # Buttons control via mause
-w = Button(window, text=" W ", command="", width=4).grid(row=2, column=1)
-a = Button(window, text=" A ", command="", width=4).grid(row=3, column=0)
-s = Button(window, text=" S ", command="", width=4).grid(row=3, column=1)
-d = Button(window, text=" D ", command="", width=4).grid(row=3, column=2)
+w = Button(buttons_group, text=" ▲ ", command="", width=4, font=("Helvetica", 15, "bold")).grid(row=0, column=0, columnspan=3, pady=5)
+a = Button(buttons_group, text=" ◄ ", command="", width=4, font=("Helvetica", 15, "bold")).grid(row=1, column=0, padx=3)
+s = Button(buttons_group, text=" ▼ ", command="", width=4, font=("Helvetica", 15, "bold")).grid(row=1, column=1, padx=3)
+d = Button(buttons_group, text=" ► ", command="", width=4, font=("Helvetica", 15, "bold")).grid(row=1, column=2, padx=3)
 
 # snake function
 def snake():
