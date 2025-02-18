1ab0cd209e4d9651c20772554d9914c4a6ab6c28
Tayger84 <143450458+Tayger84@users.noreply.github.com>
2025-02-18 22:20:09 +0100

    move in direction

--- snake.py
+++ snake.py
@@ -5,7 +5,7 @@ from snake_body import Snake, Fruit
 window = Tk()
 window.configure(bg="#73f094")
 window.title("Snake Game")
-window.iconbitmap("icons\snake.ico")
+window.iconbitmap("snake.ico")
 window.resizable(False, False)
 
 # score label for actual state of score
@@ -21,17 +21,19 @@ canvas.grid(row=1, padx=10, pady=10)
 buttons_group = Frame(window, bg="#73e090")
 buttons_group.grid(row=2, pady=10)
 
-# Buttons control via mause
-w = Button(buttons_group, text=" ▲ ", command="", width=4, font=("Helvetica", 15, "bold")).grid(row=0, column=0, columnspan=3, pady=5)
-a = Button(buttons_group, text=" ◄ ", command="", width=4, font=("Helvetica", 15, "bold")).grid(row=1, column=0, padx=3)
-s = Button(buttons_group, text=" ▼ ", command="", width=4, font=("Helvetica", 15, "bold")).grid(row=1, column=1, padx=3)
-d = Button(buttons_group, text=" ► ", command="", width=4, font=("Helvetica", 15, "bold")).grid(row=1, column=2, padx=3)
-
 # snake initialization
 snake = Snake(canvas, window)
 #snake.snake_move(10, 0)
 
 snake.move()
+
+# Buttons control via mause
+w = Button(buttons_group, text=" ▲ ", command=snake.move_up, width=4, font=("Helvetica", 15, "bold")).grid(row=0, column=0, columnspan=3, pady=5)
+a = Button(buttons_group, text=" ◄ ", command=snake.move_left, width=4, font=("Helvetica", 15, "bold")).grid(row=1, column=0, padx=3)
+s = Button(buttons_group, text=" ▼ ", command=snake.move_down, width=4, font=("Helvetica", 15, "bold")).grid(row=1, column=1, padx=3)
+d = Button(buttons_group, text=" ► ", command=snake.move_right, width=4, font=("Helvetica", 15, "bold")).grid(row=1, column=2, padx=3)
+
+
 
 
 
--- snake_body.py
+++ snake_body.py
@@ -11,7 +11,23 @@ class Snake:
 
     def move(self):
         self.canvas.move(self.snake, self.dx, self.dy)
-        self.canvas.after(800, self.move)
+        self.canvas.after(700, self.move)
+
+    def move_up(self):
+        self.dx = 0
+        self.dy = -10
+
+    def move_down(self):
+        self.dx = 0
+        self.dy = 10
+
+    def move_left(self):
+        self.dx = -10
+        self.dy = 0
+
+    def move_right(self):
+        self.dx = 10
+        self.dy = 0
 
        
 
